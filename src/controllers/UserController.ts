b7cd72396463fa0857814bb47469bc03968aac6b
João Vitor Santos <joaooviitorr@hotmail.com>
2021-02-24 10:42:00 -0300

    Day 03; Created users and surveys repository; Created surveys migration; Created surveys model; Created routes for list and save surveys; Created controllers for save and list surveys;

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,21 +1,27 @@
 import { Request, Response } from "express";
-import { getRepository } from "typeorm";
-import { User } from "../models/User";
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
 
 class UserController {
 	async create(request: Request, response: Response) {
 		const { name, email } = request.body;
-		const usersRepository = getRepository(User);
+
+		const usersRepository = getCustomRepository(UsersRepository);
+
 		const userAlreadyExists = await usersRepository.findOne({
 			email
 		});
+
 		if (userAlreadyExists) {
 			return response.status(400).json({
 				error: 'User already exists!'
 			})
 		}
+
 		const user = usersRepository.create({ name, email });
+
 		await usersRepository.save(user);
+
 		return response.status(201).json(user);
 	}
 }
